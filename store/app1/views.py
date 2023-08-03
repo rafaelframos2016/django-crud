2c15412cc44eb72601bc095aefd94d5e72536b87
Rafael Ramos <rafaelramos@MacBook-Air-de-Rafael.local>
2023-08-03 17:45:00 -0300

    adicionando o criar

--- store/app1/views.py
+++ store/app1/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import User
+from .forms import UserFrom
 
 # Create your views here.
+def index(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request,'index.html',context)
+
+def criar(request):
+    if request.method == 'GET':
+        form = UserFrom()
+        context = {
+            'form':form
+        }
+        return render(request,'criar.html',context)
+    else:
+        form = UserFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            users = User.objects.all()
+            context = {
+                'users': users
+            }      
+            return render(request,'index.html',context)
